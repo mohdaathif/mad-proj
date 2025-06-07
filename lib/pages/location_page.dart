dde83291acc7186d409600c8053af6ebda8487ed
mohdaathif <mohdaathif23@gmail.com>
2025-06-07 20:59:14 +0530

    Started incorporation of a location page w/google_maps_flutter & geolocator package

--- lib/pages/location_page.dart
+++ lib/pages/location_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class LocationPage extends StatefulWidget {
   const LocationPage({super.key});
@@ -8,8 +9,14 @@ class LocationPage extends StatefulWidget {
 }
 
 class _LocationPageState extends State<LocationPage> {
+  static const LatLng _pGooglePlex = LatLng(37.4223, -122.0848);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(target: _pGooglePlex, zoom: 13),
+      ),
+    );
   }
 }
